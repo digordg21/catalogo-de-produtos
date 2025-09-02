80f577c4dd0acb3d52af1ecc45821b1ec2909d3c
Rodrigo Abed de Andrade <rodrigo.abed94@gmail.com>
2025-09-02 14:32:12 -0300

    Criando modelo de autenticação -task6

---
+++ Models/CadastroDTO.cs
@@ -0,0 +1,10 @@
+namespace Catalogo.Models;
+
+public class CadastroDTO
+{
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Senha { get; set; }
+}
